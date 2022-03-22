ab57b64b8dd709d422c547986ef72fcaca879763
Baldwin <cbaldwin@FVFZMYCQJ1WK.tld>
2022-03-22 14:19:11 -0500

    week1modified

---
+++ app.js
@@ -0,0 +1,9 @@
+var name = prompt("What is your name?")
+
+alert(`Hello ${name}`)
+
+var celsius = prompt("What is the temperature in celsius?")
+
+var farenheit = (((celsius * 9) / 5) + 32)
+
+alert(`${name}, ${celsius} Cº is ${farenheit} Fº`)
